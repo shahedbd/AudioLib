2b772a12320bfda81ebef8d5f0cd90dd809f09d0
R M Shahidul Islam <shahedbddev@gmail.com>
2024-02-27 17:32:13 +0600

    + project code added

---
+++ AudioAPI/Controllers/AudioController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AudioAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AudioController : Controller
+    {
+        private readonly string _audioFolderPath;
+        public AudioController()
+        {
+            _audioFolderPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "audio");
+        }
+        [HttpGet("{fileName}")]
+        public IActionResult GetAudio(string fileName)
+        {
+            try
+            {
+                var filePath = Path.Combine(_audioFolderPath, fileName);
+                if (!System.IO.File.Exists(filePath))
+                {
+                    return NotFound();
+                }
+
+                var _FileStream = new FileStream(filePath, FileMode.Open);
+                return File(_FileStream, "audio/mpeg");
+            }
+            catch (Exception ex)
+            {
+                return new JsonResult(ex.Message);
+                throw;
+            }
+        }
+    }
+}
---
+++ AudioAPI/Program.cs
@@ -0,0 +1,26 @@
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("Open", builder => builder.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
+});
+
+
+
+var app = builder.Build();
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.MapControllers();
+app.UseCors("Open");
+app.Run();
+
+
